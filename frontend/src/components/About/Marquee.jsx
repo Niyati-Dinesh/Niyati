e14da42d52e74bef18844436f5e971d7d3185bca
Niyati-Dinesh <niyati2dinesh@gmail.com>
2025-12-22 17:16:58 +0530

    Fixed about

--- frontend/src/components/About/Marquee.jsx
+++ frontend/src/components/About/Marquee.jsx
@@ -4,21 +4,19 @@ import img2 from "/marquee/img2.jpeg";
 import img3 from "/marquee/img3.jpeg";
 import img4 from "/marquee/img4.jpeg";
 import img5 from "/marquee/img5.jpeg";
-import img6 from "/marquee/img6.jpeg";
-import img7 from "/marquee/img7.jpeg";
-import img8 from "/marquee/img8.jpeg";
+
 
 export default function Marquee() {
   return (
     <div className="marquee">
       <div className="marquee-track">
-        <img src={img2} />
+        <img src={img5} />
 
         <img src={img3} />
 
-        <img src={img4} />
-
         <img src={img5} />
+
+        <img src={img2} />
 
         {/*Duplicates the same image*/}
 
